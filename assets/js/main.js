8434be811b5417c709ed2d4d17fa73cba8062a0e
contactReleasedInfo <contact@released.info>
2021-06-06 10:31:04 +0000

    Update html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,20 @@
 $(function () {
 
+    // enter on subscribe input
+    $('#mce-EMAIL').on('keyup', function (e) {
+        var email = $(this).val();
+        var regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (regex.test(email.toLowerCase())) {
+            $('#mc-embedded-subscribe').attr('disabled', false);
+            var keycode = (e.keyCode ? e.keyCode : e.which);
+            if (keycode == '13') {
+                $('#mc-embedded-subscribe').click()
+            }
+            return
+        }
+        $('#mc-embedded-subscribe').attr('disabled', true);
+    });
+
     // reset search
     var resetSearch = function () {
         $('#search-input').val('')
--- assets/js/technology.js
+++ assets/js/technology.js
@@ -1,5 +1,20 @@
 $(function () {
 
+    // enter on subscribe input
+    $('#mce-EMAIL').on('keyup', function (e) {
+        var email = $(this).val();
+        var regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (regex.test(email.toLowerCase())) {
+            $('#mc-embedded-subscribe').attr('disabled', false);
+            var keycode = (e.keyCode ? e.keyCode : e.which);
+            if (keycode == '13') {
+                $('#mc-embedded-subscribe').click()
+            }
+            return
+        }
+        $('#mc-embedded-subscribe').attr('disabled', true);
+    });
+
     var getMetaTag = function (metaName) {
         var metas = document.getElementsByTagName('meta');
         for (let i = 0; i < metas.length; i++) {
